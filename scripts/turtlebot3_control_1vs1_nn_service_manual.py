d67800691f0ae1f9e4ecc507cded6304d16b37ed
Autolab2024 <>
2024-09-02 16:40:50 -0700

    Hanyang: add one file using HARLT method and one file prepared for rarl methods

--- scripts/turtlebot3_control_1vs1_nn_service_manual.py
+++ scripts/turtlebot3_control_1vs1_nn_service_manual.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import math
+import math, time
 from GridProcessing import Grid
 import rospy
 from geometry_msgs.msg import Twist, TransformStamped, PoseStamped
@@ -9,30 +9,20 @@ from message_filters import Subscriber, TimeSynchronizer, ApproximateTimeSynchro
 import numpy as np
 from utils import *
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
-from stable_baselines3 import PPO
-import torch
-import torch.nn as nn
-from gymnasium import spaces
 
-value1vs0_dub = np.load('/home/marslab/catkin_ws/src/turtlebot3_controller/scripts/values/1vs0Dubin_easier.npy')
-grid1vs0_dub: Grid = Grid(np.array([-1.1, -1.1, -math.pi]), np.array([1.1, 1.1, math.pi]), 3, np.array([100, 100, 200]), [2])
+#TODO: Hanyang not started yet, 2024.9.1
+start = time.time()
 
-value1vs1_dub = np.load('/home/marslab/catkin_ws/src/turtlebot3_controller/scripts/values/1vs1Dubin_easier.npy')
-grid1vs1_dub: Grid = Grid(np.array([-1.1, -1.1, -math.pi, -1.1, -1.1, -math.pi]), np.array([1.1, 1.1, math.pi, 1.1, 1.1, math.pi]), 
-                        6, np.array([28, 28, 28, 28, 28, 28]), [2, 5])
+value1vs0_dub = np.load('/home/marslab/catkin_ws/src/turtlebot3_controller/scripts/values/DubinCar1vs0_grid100_medium_0.4angularv_20hz_1.0map.npy')
+grid1vs0_dub: Grid = Grid(np.array([-1.0, -1.0, -math.pi]), np.array([1.0, 1.0, math.pi]), 3, np.array([100, 100, 200]), [2])
 
-# load the trained model
-trained_model = "/home/marslab/catkin_ws/src/turtlebot3_controller/scripts/dubin_HARLT.zip"
-action_lower_bound = np.array([-1.0])
-action_upper_bound = np.array([+1.0])
-obs_lower_bound = np.array([-1.0, -1.0, -np.pi, -1.0, -1.0, -np.pi])
-obs_upper_bound= np.array([+1.0, +1.0, np.pi, 1.0, +1.0, np.pi])
-obs_space = spaces.Box(low=obs_lower_bound, high=obs_upper_bound, dtype=np.float32)
-action_space = spaces.Box(low=action_lower_bound, high=action_upper_bound, dtype=np.float32)
+value1vs1_dub = np.load('/home/marslab/catkin_ws/src/turtlebot3_controller/scripts/values/DubinCar1vs1_grid28_medium_0.4angularv_ctrl20hz_2.0map.npy')
+grid1vs1_dub: Grid = Grid(np.array([-2.0, -2.0, -math.pi, -2.0, -2.0, -math.pi]), 
+                          np.array([2.0, 2.0, math.pi, 2.0, 2.0, math.pi]), 
+                          6, np.array([28, 28, 28, 28, 28, 28]), [2,5])
 
-model = PPO.load(trained_model, custom_objects={'observation_space':obs_space, 'action_space':action_space})
-print("========== The model has been loaded. ==========")
-
+end = time.time()
+print(f"============= HJ value functions loaded Successfully! (Time: {end-start :.4f} seconds) =============")
 rospy.loginfo("Value functions has been loaded.")
 
 
@@ -87,22 +77,20 @@ def vicon_data_callback():
         current_attacker_state = np.array(filterd_attacker)
         current_defender_state = np.array(filterd_defender)
 
-        # joint_1vs1 = (current_attacker_state[0], current_attacker_state[1], current_attacker_state[2],
-        #             current_defender_state[0], current_defender_state[1], current_defender_state[2])
-        # joint_1vs1_slice = grid1vs1_dub.get_index(joint_1vs1)
-        # rospy.loginfo(f"The current value function for Defender is {value1vs1_dub[joint_1vs1_slice]}.\n")
+        joint_1vs1 = (current_attacker_state[0], current_attacker_state[1], current_attacker_state[2],
+                    current_defender_state[0], current_defender_state[1], current_defender_state[2])
+        joint_1vs1_slice = grid1vs1_dub.get_index(joint_1vs1)
+        rospy.loginfo(f"The current value function for Defender is {value1vs1_dub[joint_1vs1_slice]}.\n")
         
+
         # attacker control
         # attacker_state_slice = grid1vs0_dub.get_index(current_attacker_state)
         # value1vs0_dubs = value1vs0_dub[..., 0]
         control_attacker = hj_controller_1vs0(value1vs0_dub, grid1vs0_dub, current_attacker_state)
-        # control_attacker = np.array([[0.0]])
         # rospy.loginfo(f"The control command of the attacker is {control_attacker}. \n")
 
         # defender control
-        obs = np.concatenate((filterd_attacker, filterd_defender))
-        control_defender, _ = model.predict(obs, deterministic=True)
-        # control_defender = hj_contoller_defenders_dub_1vs1(current_attacker_state, current_defender_state, value1vs1_dub, grid1vs1_dub)
+        control_defender = hj_contoller_defenders_dub_1vs1(current_attacker_state, current_defender_state, value1vs1_dub, grid1vs1_dub)
         rospy.loginfo(f"The control command of the defender is {control_defender}. \n")
 
 
@@ -125,9 +113,9 @@ def vicon_data_callback():
             attacker_cmd.linear.x = 0.22  # 1.0
             attacker_cmd.angular.z = control_attacker[0][0]
             defender_cmd.linear.x = 0.22
-            defender_cmd.angular.z = control_defender[0]
+            defender_cmd.angular.z = control_defender[0][0]
 
-        attacker_pub.publish(attacker_cmd)
+        # attacker_pub.publish(attacker_cmd)
         defender_pub.publish(defender_cmd)
 
         rospy.loginfo("-----------")
@@ -136,9 +124,9 @@ def vicon_data_callback():
 
 
 if __name__ == "__main__":
-    rospy.init_node("hj_controller_nn_1vs1")
+    rospy.init_node("hj_controller_1vs1")
 
-    rospy.loginfo("Initialize the NN controller for 1vs1.")
+    rospy.loginfo("Initialize the hj controller for 1vs1.")
     # rate = rospy.Rate(200)
 
     agentLocation = rospy.ServiceProxy('/vicon/grab_vicon_pose/', viconGrabPose)
